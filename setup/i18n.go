e3c58b1c36fb9e9f471e964659d2d87d18d0576b
zdt1013 <zdt1013@qq.com>
2025-04-16 13:43:57 +0800

    🌐 i18n(i18n.go): 为i18n.go国际化辅助类添加注释。
    - 为i18n.go文件中的国际化辅助类添加了详细的注释，以便于理解和维护。

--- setup/i18n.go
+++ setup/i18n.go
@@ -2,23 +2,27 @@ package setup
 
 import "github.com/gookit/i18n"
 
+// I18n 定义了所有需要国际化的文本内容
 type I18n struct {
-	UserInput        string
-	UserInputFormat  string
-	ChatHelp         string
-	SelectOperation  string
-	FillParamsAndRun string
-	AdjustAndRun     string
-	YourChoice       string
-	RunNow           string
-	CanExecute       string
-	Exit             string
-	ParamEmptyError  string
-	UrlInvalidError  string
+	UserInput        string // 用户输入提示
+	UserInputFormat  string // 用户输入格式提示
+	ChatHelp         string // 聊天帮助信息
+	SelectOperation  string // 选择操作提示
+	FillParamsAndRun string // 填充参数并运行提示
+	AdjustAndRun     string // 调整并运行提示
+	YourChoice       string // 你的选择提示
+	RunNow           string // 立即运行提示
+	CanExecute       string // 可以执行提示
+	Exit             string // 退出提示
+	ParamEmptyError  string // 参数为空错误
+	UrlInvalidError  string // URL无效错误
 }
 
+// i18nInstance 是 I18n 的单例实例
 var i18nInstance *I18n
 
+// GetI18n 获取 I18n 的单例实例
+// 如果实例不存在则初始化一个新实例
 func GetI18n() *I18n {
 	if i18nInstance == nil {
 		i18nInstance = &I18n{
@@ -39,6 +43,7 @@ func GetI18n() *I18n {
 	return i18nInstance
 }
 
+// getDtr 获取指定key的国际化文本
 func getDtr(key string) string {
 	return i18n.Dtr(key)
 }
